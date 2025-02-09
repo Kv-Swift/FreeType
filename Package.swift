3270c208e1d3d4550c0eecb04f047c936af6bc05
kivyswiftlink <kivyswiftlink@gmail.com>
2025-02-09 00:32:37 +0000

    Update from https://github.com/kivyswiftlink/CoreBuilder/commit/8bdf080f902ad86495c21a91aded8219a466f07f

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "libfreetype",
-            url: "https://github.com/kivyswiftlink/FreeType/releases/download/311.0.11/libfreetype.zip",
-            checksum: "b6ce5d3ae8bbfad4332529b379974f177b4a8f0860f3347d1d48d0dddd06b6c8"
+            url: "https://github.com/kivyswiftlink/FreeType/releases/download/311.0.12/libfreetype.zip",
+            checksum: "2c76cb73342664ce580e8727dda7a46c0629b6695c1a915164cde6a582910a3f"
         )
     ]
 )
