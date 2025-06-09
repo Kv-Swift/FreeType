98794e09d77696804b9c6dd12987d0aa6c6aba58
kv-swift <kivyswiftlink@gmail.com>
2025-06-09 19:44:35 +0000

    Update from https://github.com/Kv-Swift/CoreBuilder/commit/a9ef29a0c0ad8c2e321e3995803eb8ca1eb7b6e0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "libfreetype",
-            url: "https://github.com/kv-swift/FreeType/releases/download/311.1.16/libfreetype.zip",
-            checksum: "f060a122d33a8d73923b7f51bc9f5379ba3502525f5cd3b6072267656b5ad1b3"
+            url: "https://github.com/kv-swift/FreeType/releases/download/311.1.17/libfreetype.zip",
+            checksum: "d20f324b2bb0bbe4471a9af34e4e08a432d0754a65207915481dc2efee2cdcd5"
         )
     ]
 )
